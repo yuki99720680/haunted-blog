2ee5ebafdb7ae5ff75fbcb4c974693da2565cc15
m-kimura <mk.alstroemeria@gmail.com>
2022-06-10 16:28:18 +0900

    [fix] 誰でも有料機能を使える問題を修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -52,6 +52,10 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    if current_user.premium
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 end
